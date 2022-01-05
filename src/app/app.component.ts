6494b999276fa27be4dab694de3b1bb3454d5b99
dinixweb <sd4live@gmail.com>
2022-01-05 15:38:40 +0100

    html structure finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+
+  password = '';
+  
+  onButtonClick() {
+    console.log('buton was clicked');
+ }
 }
